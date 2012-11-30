b6ead92f037a4a499fd1ca67853bb46dd596f500
Jonathan Christopher <jonathan@irontoiron.com>
2012-11-30 14:00:07 -0500

    Added sanitization to field attributes

--- classes/fields/class.field.text.php
+++ classes/fields/class.field.text.php
@@ -7,28 +7,24 @@
  * @subpackage Main
  */
 
-if ( !class_exists( 'Attachments_Field_Text' ) ) :
+class Attachments_Field_Text extends Attachments_Field implements Attachments_Field_Template
+{
 
-    class Attachments_Field_Text extends Attachments_Field implements Attachments_Field_Template
+    function __construct( $name = 'name', $label = 'Name', $value = null )
     {
-
-        function __construct( $name = 'text', $label = 'Text', $value = null )
-        {
-            parent::__construct( $name, $label, $value );
-        }
-
-        function html( $field )
-        {
-        ?>
-            <input type="text" name="<?php echo $field->field_name; ?>" id="<?php echo $field->field_id; ?>" class="attachments attachments-field attachments-field-<?php echo $field->field_name; ?> attachments-field-<?php echo $field->field_id; ?>" value="<?php echo $field->value; ?>" />
-        <?php
-        }
-
-        function format_value_for_input( $value, $field = null  )
-        {
-            return htmlspecialchars( $value, ENT_QUOTES );
-        }
-
+        parent::__construct( $name, $label, $value );
     }
 
-endif; // class_exists check
+    function html( $field )
+    {
+    ?>
+        <input type="text" name="<?php esc_attr_e( $field->field_name ); ?>" id="<?php esc_attr_e( $field->field_id ); ?>" class="attachments attachments-field attachments-field-<?php esc_attr_e( $field->field_name ); ?> attachments-field-<?php esc_attr_e( $field->field_id ); ?>" value="<?php esc_attr_e( $field->value ); ?>" />
+    <?php
+    }
+
+    function format_value_for_input( $value, $field = null  )
+    {
+        return htmlspecialchars( $value, ENT_QUOTES );
+    }
+
+}
